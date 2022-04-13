b78e6f66a2d695a74f4393085f9c319b5c8869fd
egemen <emaras@ncsu.edu>
2022-04-13 16:51:22 -0400

    added scram execution capability through docker

--- __main__.py
+++ __main__.py
@@ -1,9 +1,9 @@
 import os
 import shutil
-# from scripts import fault_tree_generator
 import re
 
 def prepare_xfta(name):
+
     with open('xfta.template', 'r') as file:
         filedata = file.read()
     filedata = re.sub("NAME", name, filedata)
@@ -11,27 +11,33 @@ def prepare_xfta(name):
         file.write(filedata)
 
 def run_xfta(xfta_config_name):
+
     xfta_exec_dir = "C:/Users/emaras/Desktop/xfta/xfta-1-3-1-win32/"
     command_to_run_xfta = xfta_exec_dir + "xftar " + xfta_config_name
     print(command_to_run_xfta)
     os.system(command_to_run_xfta)
 
+def run_scram(scram_input):
+    scram_exec_command = "docker run -it --rm -v " + os.getcwd() + "/codes/scram:/scram supra scram /scram/" + scram_input + ".xml --bdd > codes/scram/" + scram_input + ".txt"
+    print(scram_exec_command)
+    os.system(scram_exec_command)
+    print(scram_input + '.xml kostu')
+
 def generate_multiple_ft():
 
     num_basic = 100
-    while (num_basic <= 300):
+    while (num_basic <= 1000):
         command_to_generate = "python scripts/fault_tree_generator.py -b " + str(num_basic) + " -o codes/scram/ft_" + str(num_basic) + ".xml"
         # generate inputs in scram directory
         os.system(command_to_generate)
+        run_scram("ft_" + str(num_basic))
         # copy ft inputs to xfta directory
         shutil.copy("codes/scram/ft_" + str(num_basic) + ".xml", "codes/xfta/ft_" + str(num_basic) + ".xml")
         # generate xfta config file and move
         prepare_xfta("ft_"+str(num_basic))
         os.replace("xfta_ft_"+str(num_basic)+".xml", "codes/xfta/xfta_ft_"+str(num_basic)+".xml")
         # rest
-        print(os.getcwd() + "/codes/xfta/xfta_ft_" + str(num_basic) + ".xml")
         run_xfta(os.getcwd() + "/codes/xfta/xfta_ft_" + str(num_basic) + ".xml")
-        # print(command_to_generate)
         num_basic += 100
 
 if __name__ == '__main__':
