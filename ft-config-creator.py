a3dcf8f27f1b175341fec4a9d1de7617008c920e
egemen <emaras@ncsu.edu>
2022-07-08 17:47:52 -0400

    Initial logic is ready for fault tree input file generation and quantification including test step

---
+++ ft-config-creator.py
@@ -0,0 +1,41 @@
+"""
+{Egemen M. Aras}
+This script creates fault-tree(s) and config files necessary for benchmarking
+"""
+
+"""Generates openpsa .xml format fault trees and store them in the openpsa directory"""
+def ftGenOpenpsa():
+    pass
+
+
+"""Generates SAPHIRE .JSInp format fault trees and store them in the saphsolve directory"""
+def ftGenSaphire():
+    pass
+
+
+"""Generates config files for XFTA in .xml format and store them in the xfta-config directory"""
+def configFileGenXFTA():
+    pass
+
+
+"""Generates config files for Benchexec in .xml format and store them in the benchexec-config directory"""
+def configFileGenBenchexec():
+    pass
+
+
+#<======================================================================================>#
+"""test-run"""
+def runXFTA():
+    pass
+
+def runSCRAM():
+    pass
+
+def runSAPHSOLVE():
+    pass
+
+
+#<======================================================================================>#
+"""compare-results"""
+def getResults():
+    pass
