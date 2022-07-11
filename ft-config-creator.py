81280123e4ed5ba3710bd45143e9f6111b17c4a8
egemen <emaras@ncsu.edu>
2022-07-11 18:33:22 -0400

    congig file generator for xfta is added and directories are organised

--- ft-config-creator.py
+++ ft-config-creator.py
@@ -2,21 +2,18 @@
 {Egemen M. Aras}
 This script creates fault-tree(s) and config files necessary for benchmarking
 """
+import os
 
+commandToRun = ""
 """Generates openpsa .xml format fault trees and store them in the openpsa directory"""
 def ftGenOpenpsa():
-    pass
-
+    commandToRun = "python scripts/ft-gen-openpsa.py"
+    os.system(commandToRun)
 
 """Generates SAPHIRE .JSInp format fault trees and store them in the saphsolve directory"""
 def ftGenSaphire():
-    pass
-
-
-"""Generates config files for XFTA in .xml format and store them in the xfta-config directory"""
-def configFileGenXFTA():
-    pass
-
+    commandToRun = "python scripts/ft-gen-saphsolve.py"
+    os.system(commandToRun)
 
 """Generates config files for Benchexec in .xml format and store them in the benchexec-config directory"""
 def configFileGenBenchexec():
@@ -25,11 +22,16 @@ def configFileGenBenchexec():
 
 #<======================================================================================>#
 """test-run"""
-def runXFTA():
-    pass
+def runXFTA(XFTAConfigName):
+    XFTAExecDir = "C:/Users/emaras/Desktop/Repo/xfta/xfta-1-3-1-win32/xftar"
+    commandToRunXFTA = XFTAExecDir + "xftar " + XFTAConfigName
+    print(commandToRunXFTA)
+    os.system(commandToRunXFTA)
 
-def runSCRAM():
-    pass
+def runSCRAM(inputFile):
+    SCRAMExecCommand = "docker run -it --rm -v " + os.getcwd() + "/models/scram:/scram supra scram /scram/" + inputFile + ".xml --mocus --probability --importance > models/scram/" + inputFile + ".txt"
+    print(SCRAMExecCommand)
+    os.system(SCRAMExecCommand)
 
 def runSAPHSOLVE():
     pass
@@ -39,3 +41,15 @@ def runSAPHSOLVE():
 """compare-results"""
 def getResults():
     pass
+
+
+if __name__ == '__main__':
+    if not os.path.exists("models/scram"):
+        os.makedirs("models/scram")
+    if not os.path.exists("models/xfta"):
+        os.makedirs("models/xfta")
+    if not os.path.exists(("models/saphsolve")):
+        os.makedirs("models/saphsolve")
+    ftGenOpenpsa()
+    ftGenSaphire()
+    runXFTA()
